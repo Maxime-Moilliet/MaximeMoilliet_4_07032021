44f625bd4a01f53789c31c66f70e0da7ec949e5e
Maxime Moilliet <mllt.dev66@gmail.com>
2021-03-31 17:38:06 +0200

    ajout des arias

--- js/ReturnTop.js
+++ js/ReturnTop.js
@@ -1,18 +1,19 @@
 class ReturnTop {
   constructor() {
     document.addEventListener("scroll", this.BuildReturnTop);
-}
+  }
 
   BuildReturnTop() {
     const returnTop = document.getElementById("js-returnTop");
     const header = document.querySelector(".header");
     if (window.scrollY >= header.offsetHeight - 30) {
       returnTop.style.display = "block";
+      returnTop.setAttribute("aria-hidden", "false");
     } else if (window.scrollY < header.offsetHeight - 30) {
-      returnTop.style.display = "none"
+      returnTop.style.display = "none";
+      returnTop.setAttribute("aria-hidden", "true");
     }
   }
-
 }
 
 export { ReturnTop };
